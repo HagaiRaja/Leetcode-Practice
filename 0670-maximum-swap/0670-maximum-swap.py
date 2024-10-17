7583960de6f05b2cb6d8ef74ec5a1478e8e35cf7
Hagai Raja Sinulingga <37261965+HagaiRaja@users.noreply.github.com>
2024-10-17 12:16:37 +0100

    Time: 33 ms (69.55%), Space: 16.6 MB (10.8%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        digits = [c for c in str(num)]
+        stack = [[int(digits[0]), 0, 0]]
+        for i in range(1, len(digits)):
+            cur = int(digits[i])
+            cur_dst = i
+            while len(stack):
+                val, src, dst = stack.pop()
+                if val > cur:
+                    stack.append([val, src, dst])
+                    stack.append([cur, i, cur_dst])
+                    break
+                elif val < cur:
+                    cur_dst = dst
+                    if len(stack) == 0: # the last
+                        stack.append([cur, i, cur_dst])
+                        break
+                else: # val == cur
+                    if src == dst:
+                        stack.append([val, src, dst])
+                        if cur_dst != i:
+                            stack.append([cur, i, cur_dst])
+                    else:
+                        cur_dst = dst
+                        stack.append([cur, i, cur_dst])
+                    break
+        
+        for i in range(len(stack)):
+            if stack[i][1] != stack[i][2]:
+                break
+        
+        print(stack)
+        if i == len(stack): return num
+        digits[stack[i][1]], digits[stack[i][2]] = digits[stack[i][2]], digits[stack[i][1]]
+        return int("".join(digits))
