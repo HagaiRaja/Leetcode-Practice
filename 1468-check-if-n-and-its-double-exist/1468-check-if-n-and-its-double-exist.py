3acdfe53c9c4c245cf74fcb2800cea105e8c4a25
Hagai Raja Sinulingga <37261965+HagaiRaja@users.noreply.github.com>
2024-12-01 17:32:57 +0000

    Time: 4 ms (42.81%), Space: 17.4 MB (5.25%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -1,9 +1,8 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        count0 = sum([0 if a else 1 for a in arr])
-        if count0 > 1: return True
-        arr = set(arr)
-        if count0: arr.remove(0)
+        arr = Counter(arr)
+        if arr[0] > 1: return True
+        del arr[0]
         for c in arr:
             if c*2 in arr:
                 return True
