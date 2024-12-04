519d925721886152dae5695cd83ebe5e0feefcfc
Hagai Raja Sinulingga <37261965+HagaiRaja@users.noreply.github.com>
2024-12-04 00:26:12 +0000

    Time: 77 ms (58.33%), Space: 17.5 MB (56.4%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
@@ -1,14 +1,14 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        def get_next_chr(ch):
-            i = ord(ch) + 1
-            if i == 123: i = 97
-            return chr(i)
+        next_ch = {}
+        for i in range(97, 122):
+            next_ch[chr(i)] = chr(i+1)
+        next_ch['z'] = 'a'
         
         i, j = 0, 0
 
         while i < len(str1):
-            if str1[i] == str2[j] or get_next_chr(str1[i]) == str2[j]:
+            if str1[i] == str2[j] or next_ch[str1[i]] == str2[j]:
                 j += 1
                 if j == len(str2): return True
             i += 1
