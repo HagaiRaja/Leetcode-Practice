505f50176af1549d536283616d8b3e5bbec5349c
Hagai Raja Sinulingga <37261965+HagaiRaja@users.noreply.github.com>
2024-12-09 12:20:13 +0000

    Time: 395 ms (5.09%), Space: 46.6 MB (42.51%) - LeetHub

---
+++ 3427-special-array-ii/3427-special-array-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        bads = []
+        for i in range(len(nums)-1):
+            if (nums[i] & 1) == (nums[i+1] & 1):
+                bads.append(i)
+
+        def get_idx(val):
+            l, r = 0, len(bads)
+            while l < r:
+                mid = (l+r)//2
+                if bads[mid] > val:
+                    r = mid
+                elif bads[mid] < val:
+                    l = mid + 1
+                else:
+                    l = mid
+                    break
+            return l
+        print(bads)
+        ans = []
+        for s, e in queries:
+            s1 = get_idx(s)
+            e1 = get_idx(e)
+            if e1-s1 == 0:
+                ans.append(True)
+            else:
+                ans.append(False)
+            print(s1, e1)
+        return ans
