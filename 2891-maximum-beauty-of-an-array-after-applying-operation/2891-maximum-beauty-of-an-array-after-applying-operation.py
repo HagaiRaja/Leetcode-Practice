172100c6d33ef3e8389fb14835c32e822435ef0e
Hagai Raja Sinulingga <37261965+HagaiRaja@users.noreply.github.com>
2024-12-11 17:41:44 +0000

    Time: 987 ms (12.81%), Space: 45.2 MB (9.88%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.py
@@ -2,13 +2,13 @@ class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         heap = []
         for num in nums:
-            heapq.heappush(heap, (num-k, 0))
-            heapq.heappush(heap, (num+k, 1))
+            heap.append((num-k, 0))
+            heap.append((num+k, 1))
         
+        heap.sort()
         ans = 0
         score = 0
-        while len(heap):
-            _, val = heapq.heappop(heap)
+        for _, val in heap:
             if val: score -= 1
             else: score += 1
             ans = max(ans, score)
