4fea287031c816386cd3f5d796fb2d6bb9452183
Hagai Raja Sinulingga <37261965+HagaiRaja@users.noreply.github.com>
2024-12-12 00:41:44 +0000

    Time: 3 ms (87.72%), Space: 17.3 MB (20.6%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.py
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.py
@@ -1,9 +1,10 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
+        for i in range(len(gifts)):
+            gifts[i] *= -1
+        heapq.heapify(gifts)
+
         for _ in range(k):
-            maxi = 0
-            for i in range(1, len(gifts)):
-                if gifts[i] > gifts[maxi]:
-                    maxi = i
-            gifts[maxi] = int(gifts[maxi]**0.5)
-        return sum(gifts)
+            cur = heapq.heappop(gifts)
+            heapq.heappush(gifts, -int((cur*-1)**0.5))
+        return sum(gifts)*-1
